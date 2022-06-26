c25ac8d4f42fba07d0d68c87b64f2b55dddc539f
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2022-06-26 13:24:07 -0400

    Created convenience header for engine users

---
+++ GadgetEngine/Gadget.h
@@ -0,0 +1,9 @@
+#ifndef GADGET_GADGET_H
+#define GADGET_GADGET_H
+
+//Convenience include header for engine users
+#include "App.h"
+#include "Math/Angle.h"
+#include "Math/Math.h"
+
+#endif //!GADGET_GADGET_H
