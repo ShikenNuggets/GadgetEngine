cbeb5984ba72c942341124c19372554a73d4a7f5
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2022-06-28 21:00:29 -0400

    Created a race condition detector

--- GadgetEngine/Concurrency/SpinLock.cpp
+++ GadgetEngine/Concurrency/SpinLock.cpp
@@ -19,3 +19,15 @@ bool SpinLock::TryAcquire(){
 void SpinLock::Release(){
 	atomicFlag.clear(std::memory_order_release);
 }
+
+RaceConditionDetector::RaceConditionDetector() : flag(false){}
+
+void RaceConditionDetector::Acquire(){
+	_ASSERT(!flag); //No one should already have the lock
+	flag = true;
+}
+
+void RaceConditionDetector::Release(){
+	_ASSERT(flag); //Release should only be called after Acquire
+	flag = false;
+}
--- GadgetEngine/Concurrency/SpinLock.h
+++ GadgetEngine/Concurrency/SpinLock.h
@@ -5,16 +5,39 @@
 
 namespace Gadget{
 	class SpinLock{
-	private:
-		std::atomic_flag atomicFlag;
-
 	public:
 		SpinLock();
 
 		void Acquire();
 		bool TryAcquire();
 		void Release();
+	
+	private:
+		std::atomic_flag atomicFlag;
 	};
+
+	//Lightweight spinlock used specifically for detecting race conditions
+	//Use this to confirm if you need an actual lock or not
+	//Use the macros defined below so they aren't included in Release builds
+	class RaceConditionDetector{
+	public:
+		RaceConditionDetector();
+
+		void Acquire();
+		void Release();
+
+	private:
+		volatile bool flag; //Does not need to be atomic since this is just to detect races and not prevent them. Volatile so it doesn't get optimized out
+	};
+
+#ifdef GADGET_DEBUG
+	#define BEGIN_RACE_CONDITION_DETECTION(L) (L).Acquire();
+	#define END_RACE_CONDITION_DETECTION(L) (L).Release();
+#else
+	#define BEGIN_RACE_CONDITION_DETECTION(L)
+	#define END_RACE_CONDITION_DETECTION(L)
+#endif //GADGET_DEBUG
+
 }
 
 #endif // !GADGET_SPIN_LOCK_H
