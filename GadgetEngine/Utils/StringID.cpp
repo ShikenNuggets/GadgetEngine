836acccc18dcc5a0ae68aeb6462a3275e624f881
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2022-07-16 21:38:41 -0400

    You can now get the original string from the StringID

--- GadgetEngine/Utils/StringID.cpp
+++ GadgetEngine/Utils/StringID.cpp
@@ -8,6 +8,10 @@ std::unordered_map<uint32_t, std::string> StringID::stringIdTable;
 
 constexpr StringID::StringID(uint32_t id_) : id(id_){}
 
+std::string StringID::GetString() const{
+	return GetStringFromID(*this);
+}
+
 StringID StringID::InternString(const std::string& str_){
 	StringID id = StringID(Hash::CRC32(str_.data(), str_.length()));
 
@@ -16,6 +20,15 @@ StringID StringID::InternString(const std::string& str_){
 	}
 
 	return id;
+}
+
+std::string StringID::GetStringFromID(StringID id_){
+	auto find = stringIdTable.find(id_.id);
+	if(find == stringIdTable.end()){
+		return find->second;
+	}
+
+	return "";
 }
 
 constexpr bool StringID::operator ==(StringID id_) const{ return id == id_.id; }
--- GadgetEngine/Utils/StringID.h
+++ GadgetEngine/Utils/StringID.h
@@ -9,10 +9,12 @@ namespace Gadget{
 		uint32_t id;
 
 		explicit constexpr StringID(uint32_t id_);
+		std::string GetString() const;
 
 		static std::unordered_map<uint32_t, std::string> stringIdTable; //TODO - Replace this with a custom hash table with a constexpr Find function (would allow us to intern strings at compile time)
 
 		static StringID InternString(const std::string& str_); //TODO - Once this is constexpr, connect it to a user-defined literal
+		static std::string GetStringFromID(StringID id_);
 
 		constexpr bool operator ==(StringID a_) const;
 		constexpr bool operator !=(StringID a_) const;
