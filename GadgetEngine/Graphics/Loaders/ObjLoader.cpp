7e6f32cb7f36200e27e37b581a78c4db34f129fd
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2022-09-06 22:17:28 -0400

    [WIP] Proper indexing on model loads

--- GadgetEngine/Graphics/Loaders/ObjLoader.cpp
+++ GadgetEngine/Graphics/Loaders/ObjLoader.cpp
@@ -37,7 +37,7 @@ Mesh* ObjLoader::LoadMesh(const std::string& filePath_){
 		std::string remainder = s.substr(s.find(" "), s.size());
 
 		if(firstWord == "v"){
-			//Vertex
+			//Vertex Position
 			tempPos.push_back(ProcessVector3(remainder));
 		}else if(firstWord == "vt"){
 			//Texture Coordinate
@@ -57,52 +57,39 @@ Mesh* ObjLoader::LoadMesh(const std::string& filePath_){
 		return nullptr;
 	}
 
-	//TODO - FIX THIS!!!
-	//This logic is for converting the index lists that the OBJ gives us into something graphics APIs/the engine can process
-	//For unclear reasons, this current version has the wrong winding order for *some* triangles
-	//For now we're just doing indices the most inefficient brain-dead way possible, but obviously we need this to work correctly long-term
 	std::vector<VertIndex> uniqueVerts;
-	uniqueVerts.reserve(tempIndices.size());
-	uniqueVerts.push_back(tempIndices[0]);
-
-	for(size_t i = 1; i < tempIndices.size(); i++){
-		bool isUnique = true;
-
-		//TODO - Bring back this for loop when everything's fixed
-		/*for(const auto& v : uniqueVerts){
-			if(tempIndices[i] == v){
-				isUnique = false;
-				break;
-			}
-		}*/
-
-		if(isUnique){
-			uniqueVerts.push_back(tempIndices[i]);
-		}
-	}
-
 	std::vector<Vertex> vertices;
-	vertices.reserve(uniqueVerts.size());
-	for(const auto& v : uniqueVerts){
-		vertices.push_back(Vertex(tempPos[v.pos], tempNorm[v.norm], tempTex[v.tex]));
-	}
-
 	std::vector<uint32_t> indices;
+	
+	uniqueVerts.reserve(tempIndices.size());
+	vertices.reserve(tempIndices.size());
 	indices.reserve(tempIndices.size());
 
-	//TODO - Bring back this for loop when everything's fixed
-	/*for(const auto& ti : tempIndices){
-		for(int i = 0; i < uniqueVerts.size(); i++){
-			if(ti == uniqueVerts[i]){
-				indices.push_back(i);
-				break;
-			}
-		}
-	}*/
-
-	//TODO - Remove this for loop when everything's fixed
-	for(int i = 0; i < vertices.size(); i++){
+	for(int i = 0; i < tempIndices.size(); i++){
+		vertices.push_back(Vertex(tempPos[tempIndices[i].pos], tempNorm[tempIndices[i].norm], tempTex[tempIndices[i].tex]));
 		indices.push_back(i);
+
+		//TODO - FIX THIS!!!
+		//This logic is for converting the index lists that the OBJ gives us into something graphics APIs/the engine can process
+		//For unclear reasons, this current version gives us an inconsistent winding order
+		//int uniqueIndex = -1;
+		//for(int j = 0; j < uniqueVerts.size(); j++){
+		//	if(tempIndices[i] == uniqueVerts[j]){
+		//		uniqueIndex = j;
+		//		break;
+		//	}
+		//}
+
+		//if(uniqueIndex < 0){
+		//	// < 0 means this is a unique vertex, add it to the list
+		//	uniqueVerts.push_back(tempIndices[i]);
+		//	vertices.push_back(Vertex(tempPos[tempIndices[i].pos], tempNorm[tempIndices[i].norm], tempTex[tempIndices[i].tex]));
+		//	//indices.push_back(i);
+		//	indices.push_back(vertices.size() - 1);
+		//}else{
+		//	//The vertex is already in the list, simply use the index of that vertex
+		//	indices.push_back(uniqueIndex);
+		//}
 	}
 
 	vertices.shrink_to_fit();
