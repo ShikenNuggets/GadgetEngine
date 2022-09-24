ba4b768c900ebb529fe0e996a4a4b6a762e45a69
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2022-09-24 11:35:52 -0400

    Direct State Access for MeshInfo

--- GadgetEngine/Graphics/OpenGL/GL_MeshInfo.cpp
+++ GadgetEngine/Graphics/OpenGL/GL_MeshInfo.cpp
@@ -8,11 +8,9 @@ using namespace Gadget;
 GL_MeshInfo::GL_MeshInfo(const Mesh& mesh_) : MeshInfo(), vao(0), vbo(0), ebo(0){
 	GADGET_ASSERT(App::GetInstance()->GetCurrentRenderAPI() == Renderer::API::OpenGL, "Tried to execute OpenGL commands on non-OpenGL render API!");
 
-	glGenVertexArrays(1, &vao);
-	glGenBuffers(1, &vbo);
-	glGenBuffers(1, &ebo);
-
-	Bind();
+	glCreateVertexArrays(1, &vao);
+	glCreateBuffers(1, &vbo);
+	glCreateBuffers(1, &ebo);
 
 	static constexpr GLfloat triangleVerts[] = {
 		-1.0f, -1.0f, 0.0f,
@@ -21,18 +19,26 @@ GL_MeshInfo::GL_MeshInfo(const Mesh& mesh_) : MeshInfo(), vao(0), vbo(0), ebo(0)
 	};
 	static constexpr GLuint triangleIndices[] = { 0, 1, 2 };
 
-	glBufferData(GL_ARRAY_BUFFER, mesh_.vertices.size() * sizeof(Vertex), &mesh_.vertices[0], GL_STATIC_DRAW);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh_.indices.size() * sizeof(uint32_t), &mesh_.indices[0], GL_STATIC_DRAW);
+	glNamedBufferData(vbo, mesh_.vertices.size() * sizeof(Vertex), &mesh_.vertices[0], GL_STATIC_DRAW);
+	glNamedBufferData(ebo, mesh_.indices.size() * sizeof(uint32_t), &mesh_.indices[0], GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), nullptr);
-	glEnableVertexAttribArray(0);
+	//Position
+	glEnableVertexArrayAttrib(vao, 0);
+	glVertexArrayAttribBinding(vao, 0, 0);
+	glVertexArrayAttribFormat(vao, 0, 3, GL_FLOAT, GL_FALSE, 0);
 
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)(sizeof(Vector3)));
-	glEnableVertexAttribArray(1);
+	//Normal
+	glEnableVertexArrayAttrib(vao, 1);
+	glVertexArrayAttribBinding(vao, 1, 0);
+	glVertexArrayAttribFormat(vao, 1, 3, GL_FLOAT, GL_FALSE, sizeof(Vector3));
 
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), ((GLvoid*)(sizeof(Vector3) + sizeof(Vector3))));
-	glEnableVertexAttribArray(2);
-	Unbind();
+	//Texture Coordinates
+	glEnableVertexArrayAttrib(vao, 2);
+	glVertexArrayAttribBinding(vao, 2, 0);
+	glVertexArrayAttribFormat(vao, 2, 2, GL_FLOAT, GL_FALSE, sizeof(Vector3) + sizeof(Vector3));
+
+	glVertexArrayVertexBuffer(vao, 0, vbo, 0, sizeof(Vertex));
+	glVertexArrayElementBuffer(vao, ebo);
 }
 
 GL_MeshInfo::~GL_MeshInfo(){
@@ -43,12 +49,8 @@ GL_MeshInfo::~GL_MeshInfo(){
 
 void GL_MeshInfo::Bind(){
 	glBindVertexArray(vao);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 }
 
 void GL_MeshInfo::Unbind(){
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 }
