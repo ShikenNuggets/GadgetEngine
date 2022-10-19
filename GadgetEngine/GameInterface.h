b2cdbfbbd09f8a36a83b5bf38ed54acf2b8cc54b
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2022-10-19 01:41:20 -0400

    Created GameInterface for Game project to use

---
+++ GadgetEngine/GameInterface.h
@@ -0,0 +1,21 @@
+#ifndef GADGET_GAME_INTERFACE_H
+#define GADGET_GAME_INTERFACE_H
+
+#include <string>
+#include <vector>
+
+#include "Game/Scene.h"
+
+namespace Gadget{
+	class GameInterface{
+	public:
+		GameInterface(const std::string& name_) : name(name_){}
+
+		virtual void LoadGame() = 0;
+
+	protected:
+		std::string name;
+	};
+}
+
+#endif //!GADGET_GAME_INTERFACE_H
