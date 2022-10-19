809353e7cc6c9e2aa053ea12199c360cf626a8f4
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2022-10-19 01:36:20 -0400

    Created GameLogicComponent

---
+++ GadgetEngine/Game/GameLogicComponent.h
@@ -0,0 +1,18 @@
+#ifndef GADGET_GAME_LOGIC_COMPONENT_H
+#define GADGET_GAME_LOGIC_COMPONENT_H
+
+#include "Component.h"
+
+namespace Gadget{
+	class GameLogicComponent : public Component{
+	public:
+		GameLogicComponent(GameObject* parent_) : Component(parent_){}
+		virtual ~GameLogicComponent() override{}
+
+		virtual void OnStart(){}
+		virtual void OnUpdate(float deltaTime_){}
+		virtual void OnDestroy(){}
+	};
+}
+
+#endif //!GADGET_GAME_LOGIC_COMPONENT_H
