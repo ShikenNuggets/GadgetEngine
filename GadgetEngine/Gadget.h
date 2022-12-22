bc75274a8daffa21c3914d95323cf141fe338eab
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2022-12-21 20:40:58 -0500

    Expanded the Gadget header

--- GadgetEngine/Gadget.h
+++ GadgetEngine/Gadget.h
@@ -3,7 +3,19 @@
 
 //Convenience include header for engine users
 #include "App.h"
+#include "Config.h"
+#include "Debug.h"
+#include "EngineVars.h"
+#include "Random.h"
+#include "Core/Time.h"
+#include "Game/Component.h"
+#include "Game/GameObject.h"
+#include "Game/Scene.h"
+#include "Input/Input.h"
 #include "Math/Angle.h"
 #include "Math/Math.h"
+#include "Resource/ResourceManager.h"
+#include "Utils/StringID.h"
+#include "Utils/Utils.h"
 
 #endif //!GADGET_GADGET_H
