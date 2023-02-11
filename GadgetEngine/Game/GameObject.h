119ceadf96afacf7d89511b680dfe57465863082
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2023-02-11 16:05:04 -0500

    Added `GameObject::GetComponents`

--- GadgetEngine/Game/GameObject.h
+++ GadgetEngine/Game/GameObject.h
@@ -26,7 +26,7 @@ namespace Gadget{
 
 		void AddComponent(Component* component_);
 
-		//THIS FUNCTION IS SLOW - Avoid calling it wherever possible
+		//THIS FUNCTION IS SLOW - Avoid calling it unless necessary, and cache the result when possible
 		template <class T> T* GetComponent() const{
 			static_assert(std::is_base_of<Component, T>::value, "T must inherit from Component");
 			T* comp = nullptr;
@@ -41,6 +41,23 @@ namespace Gadget{
 			}
 
 			return nullptr;
+		}
+
+		//THIS FUNCTION IS SLOW - Avoid calling it unless necessary, and cache the results whene possible
+		template <class T> std::vector<T*> GetComponents() const{
+			static_assert(std::is_base_of<Component, T>::value, "T must inherit from Component");
+			std::vector<T*> comps;
+
+			//Performance Note: dynamic casts are pretty slow, especially when they fail which will happen a lot here
+			//This seems to be the simplest way to do this generically, but one could optimize this on a per-project basis
+			for(Component* c : components){
+				comp = dynamic_cast<T*>(c);
+				if(comp != nullptr){
+					comps.push_back(c);
+				}
+			}
+
+			return comps;
 		}
 
 		StringID GetName() const{ return name; }
