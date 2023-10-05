641c6fba550af34eb8893e6d898db8f8cd01e5b8
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2023-10-05 17:52:56 -0400

    Adjusted ExampleScene to better test physics stuff

--- Game/ExampleScene.h
+++ Game/ExampleScene.h
@@ -24,21 +24,29 @@ namespace Example{
 			AddSceneComponent(new Gadget::SkyboxComponent(this, SID("Skybox")));
 
 			Gadget::GameObject* cube = new Gadget::GameObject();
+			cube->SetPosition(0.0f, 2.5f, 0.0f);
 			cube->AddComponent(new Gadget::RenderComponent(cube, SID("CubeModel"), SID("CubeTexture"), SID("DefaultShader")));
 			cube->AddComponent(new Gadget::Rigidbody(cube, 100.0f, true));
 			cube->AddComponent(new Gadget::CubeCollider(cube));
-			cube->AddComponent(new ObjectRotator(cube));
+			//cube->AddComponent(new ObjectRotator(cube));
+
+			Gadget::GameObject* floor = new Gadget::GameObject();
+			floor->SetPosition(Gadget::Vector3(0.0f, -2.5f, 0.0f));
+			floor->SetScale(Gadget::Vector3(25.0f, 0.01f, 25.0f));
+			floor->AddComponent(new Gadget::RenderComponent(floor, SID("CubeModel"), SID("CubeTexture"), SID("DefaultShader")));
+			floor->AddComponent(new Gadget::CubeCollider(floor));
 
 			Gadget::GameObject* light = new Gadget::GameObject();
-			light->SetPosition(2.0f, 1.0f, 1.0f);
+			light->SetPosition(2.0f, -1.0f, 1.0f);
 			light->AddComponent(new Gadget::PointLightComponent(light));
 
 			Gadget::GameObject* camera = new Gadget::GameObject();
-			camera->SetPosition(0.0f, 0.0f, 4.0f);
+			camera->SetPosition(0.0f, 0.0f, 8.0f);
 			camera->AddComponent(new Gadget::CameraComponent(camera));
 			camera->AddComponent(new CameraController(camera));
 
 			CreateObject(cube);
+			CreateObject(floor);
 			CreateObject(light);
 			CreateObject(camera);
 		}
