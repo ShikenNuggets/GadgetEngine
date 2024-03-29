b2cf7e06ed32bb32ddbcd5c8defcfe37432c699e
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2024-03-28 21:56:42 -0400

    Fixed various oversights

---
+++ GadgetEngine/Utils/Profiler.cpp
@@ -0,0 +1,89 @@
+#include "Profiler.h"
+
+using namespace Gadget;
+
+//----------------------------------------------------------------------------------------------------//
+//---------- Timer -----------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------//
+
+Timer::Timer() : startTime(0), times(){}
+
+void Timer::StartProfiling(){
+	if(startTime.count() != 0){
+		Debug::Log(SID("UTILS"), "Tried to restart profiling, but it was never stopped!", Debug::Warning, __FILE__, __LINE__);
+		return;
+	}
+
+	startTime = CurrentTime();
+}
+
+double Timer::EndProfiling(){
+	if(startTime.count() == 0){
+		Debug::Log(SID("UTILS"), "Tried to end profiling, but it never started!", Debug::Warning, __FILE__, __LINE__);
+		return 0.0;
+	}
+
+	auto duration = CurrentTime() - startTime;
+	double durationSeconds = static_cast<double>(duration.count()) / 1000.0f;
+	times.push_back(durationSeconds);
+
+	startTime = std::chrono::milliseconds(0);
+
+	return durationSeconds;
+}
+
+double Timer::Total(){
+	double total = 0.0;
+	for(const auto& t : times){
+		total += t;
+	}
+
+	return total;
+}
+
+double Timer::Average(){
+	if(times.size() == 0){
+		return 0.0;
+	}
+
+	return Total() / static_cast<double>(times.size());
+}
+
+//----------------------------------------------------------------------------------------------------//
+//---------- Profiler --------------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------//
+
+std::map<StringID, Timer> Profiler::profiles;
+
+void Profiler::Start(StringID name_){
+	if(Utils::ContainsKey(profiles, name_)){
+		profiles[name_].StartProfiling();
+	}else{
+		profiles.emplace(name_, Timer());
+	}
+}
+
+double Profiler::End(StringID name_){
+	if(!Utils::ContainsKey(profiles, name_)){
+		Debug::Log(SID("UTILS"), "Tried to end profiling on [" + name_.GetString() + "], but it was never started!");
+		return 0.0;
+	}
+
+	return profiles[name_].EndProfiling();
+}
+
+double Profiler::TotalTime(StringID name_){
+	if(Utils::ContainsKey(profiles, name_)){
+		return profiles[name_].Total();
+	}
+
+	return 0.0;
+}
+
+double Profiler::GetAverage(StringID name_){
+	if(Utils::ContainsKey(profiles, name_)){
+		return profiles[name_].Average();
+	}
+
+	return 0.0;
+}
--- GadgetEngine/Utils/Profiler.h
+++ GadgetEngine/Utils/Profiler.h
@@ -10,51 +10,16 @@
 namespace Gadget{
 	class Timer{
 	public:
-		Timer() : startTime(0), times(){}
+		Timer();
 		
-		void StartProfiling(){
-			if(startTime.count() != 0){
-				Debug::Log(SID("UTILS"), "Tried to restart profiling, but it was never stopped!", Debug::Warning, __FILE__, __LINE__);
-				return;
-			}
+		void StartProfiling();
+		double EndProfiling();
 
-			startTime = CurrentTime();
-		}
-
-		double EndProfiling(){
-			if(startTime.count() == 0){
-				Debug::Log(SID("UTILS"), "Tried to end profiling, but it never started!", Debug::Warning, __FILE__, __LINE__);
-				return;
-			}
-
-			auto duration = CurrentTime() - startTime;
-			double durationSeconds = static_cast<double>(duration.count()) / 1000.0f;
-			times.push_back(durationSeconds);
-
-			startTime = std::chrono::milliseconds(0);
-
-			return durationSeconds;
-		}
-
-		double Total(){
-			double total = 0.0;
-			for(const auto& t : times){
-				total += t;
-			}
-
-			return total;
-		}
-
-		double Average(){
-			if(times.size() == 0){
-				return 0.0;
-			}
-
-			return Total() / static_cast<double>(times.size());
-		}
+		double Total();
+		double Average();
 		
 	private:
-		constexpr std::chrono::milliseconds CurrentTime(){
+		constexpr inline std::chrono::milliseconds CurrentTime() const{
 			return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now().time_since_epoch());
 		}
 
@@ -67,38 +32,11 @@ namespace Gadget{
 		DISABLE_COPY_AND_MOVE(Profiler)
 		Profiler() = delete;
 
-		static void Start(StringID name_){
-			if(Utils::ContainsKey(profiles, name_)){
-				profiles[name_].StartProfiling();
-			}else{
-				profiles.emplace(name_, Timer());
-			}
-		}
-		
-		static double End(StringID name_){
-			if(!Utils::ContainsKey(profiles, name_)){
-				Debug::Log(SID("UTILS"), "Tried to end profiling on [" + name_.GetString() + "], but it was never started!");
-				return 0.0;
-			}
+		static void Start(StringID name_);
+		static double End(StringID name_);
 
-			return profiles[name_].EndProfiling();
-		}
-
-		static double TotalTime(StringID name_){
-			if(Utils::ContainsKey(profiles, name_)){
-				return profiles[name_].Total();
-			}
-
-			return 0.0;
-		}
-		
-		static double GetAverage(StringID name_){
-			if(Utils::ContainsKey(profiles, name_)){
-				return profiles[name_].Average();
-			}
-
-			return 0.0;
-		}
+		static double TotalTime(StringID name_);
+		static double GetAverage(StringID name_);
 
 	private:
 		static std::map<StringID, Timer> profiles;
