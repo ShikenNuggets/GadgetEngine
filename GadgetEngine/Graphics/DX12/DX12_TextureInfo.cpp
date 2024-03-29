22ea8ec9e3091be220db3a949f4738129201deb5
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2024-03-29 14:39:25 -0400

    DX12_TextureInfo setup

---
+++ GadgetEngine/Graphics/DX12/DX12_TextureInfo.cpp
@@ -0,0 +1,126 @@
+#include "DX12_TextureInfo.h"
+
+using namespace Gadget;
+
+//----------------------------------------------------------------------------------------------------//
+//---------- DX12_TextureInfo ------------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------//
+
+DX12_TextureInfo::DX12_TextureInfo() : TextureInfo(), resource(nullptr), srvHandle(){}
+
+DX12_TextureInfo::DX12_TextureInfo(const DX12_TextureInitInfo& info_) : TextureInfo(), resource(nullptr), srvHandle(){
+	GADGET_BASIC_ASSERT(DX12::IsInitialized());
+	GADGET_BASIC_ASSERT(DX12::MainDevice() != nullptr);
+
+	const D3D12_CLEAR_VALUE* clearValue = nullptr;
+	if(info_.resourceDesc != nullptr && (info_.resourceDesc->Flags & D3D12_RESOURCE_FLAG_ALLOW_RENDER_TARGET || info_.resourceDesc->Flags & D3D12_RESOURCE_FLAG_ALLOW_DEPTH_STENCIL)){
+		clearValue = &info_.clearValue;
+	}
+
+	if(info_.resource != nullptr){
+		resource = info_.resource;
+	}else if(info_.heap != nullptr){
+		GADGET_BASIC_ASSERT(info_.resourceDesc != nullptr);
+
+		HRESULT result = DX12::MainDevice()->CreatePlacedResource(info_.heap, info_.allocationInfo.Offset, info_.resourceDesc, info_.initialState, clearValue, IID_PPV_ARGS(&resource));
+		if(FAILED(result)){
+			Debug::ThrowFatalError(SID("RENDER"), "ID3D12Device::CreatePlacedResource failed!", __FILE__, __LINE__);
+		}
+	}else{
+		GADGET_BASIC_ASSERT(info_.resourceDesc != nullptr);
+
+		HRESULT result = DX12::MainDevice()->CreateCommittedResource(&DX12_Helpers::DefaultHeapProperties, D3D12_HEAP_FLAG_NONE, info_.resourceDesc, info_.initialState, clearValue, IID_PPV_ARGS(&resource));
+		if(FAILED(result)){
+			Debug::ThrowFatalError(SID("RENDER"), "ID3D12Device::CreateCommittedResource failed!", __FILE__, __LINE__);
+		}
+	}
+
+	GADGET_BASIC_ASSERT(resource != nullptr);
+	srvHandle = DX12::SRVHeap().Allocate();
+	DX12::MainDevice()->CreateShaderResourceView(resource, info_.srvDesc, srvHandle.cpuHandle);
+}
+
+DX12_TextureInfo::~DX12_TextureInfo(){
+	DX12::DeferredRelease(resource);
+}
+
+//----------------------------------------------------------------------------------------------------//
+//---------- DX12_RenderTextureInfo ------------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------//
+
+DX12_RenderTextureInfo::DX12_RenderTextureInfo() : DX12_TextureInfo(), rtvHandles(), mipCount(0){}
+
+DX12_RenderTextureInfo::DX12_RenderTextureInfo(const DX12_TextureInitInfo& info_) : DX12_TextureInfo(info_), rtvHandles(), mipCount(0){
+	mipCount = resource->GetDesc().MipLevels;
+	GADGET_BASIC_ASSERT(mipCount != 0);
+	GADGET_BASIC_ASSERT(mipCount < maxMips);
+
+	D3D12_RENDER_TARGET_VIEW_DESC desc{};
+	desc.Format = info_.resourceDesc->Format;
+	desc.ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D;
+	desc.Texture2D.MipSlice = 0;
+
+	for(; desc.Texture2D.MipSlice < mipCount; desc.Texture2D.MipSlice++){
+		rtvHandles[desc.Texture2D.MipSlice] = DX12::RTVHeap().Allocate();
+		DX12::MainDevice()->CreateRenderTargetView(resource, &desc, rtvHandles[desc.Texture2D.MipSlice].cpuHandle);
+	}
+}
+
+DX12_RenderTextureInfo::~DX12_RenderTextureInfo(){
+	for(uint32_t i = 0; i < mipCount; i++){
+		DX12::RTVHeap().Free(rtvHandles[i]);
+	}
+	mipCount = 0;
+}
+
+D3D12_CPU_DESCRIPTOR_HANDLE DX12_RenderTextureInfo::RTV(uint32_t mipIndex_) const{
+	GADGET_BASIC_ASSERT(mipIndex_ < mipCount);
+	if(mipIndex_ >= mipCount){
+		Debug::Log(SID("RENDER"), "Tried to get D3D12_CPU_DESCRIPTOR_HANDLE for invalid mip index " + std::to_string(mipIndex_) + "!");
+		return D3D12_CPU_DESCRIPTOR_HANDLE{};
+	}
+
+	return rtvHandles[mipIndex_].cpuHandle;
+}
+
+//----------------------------------------------------------------------------------------------------//
+//---------- DX12_DepthBufferTextureInfo -------------------------------------------------------------//
+//----------------------------------------------------------------------------------------------------//
+
+DX12_DepthBufferTextureInfo::DX12_DepthBufferTextureInfo() : DX12_TextureInfo(), dsvHandle(){}
+
+DX12_DepthBufferTextureInfo::DX12_DepthBufferTextureInfo(DX12_TextureInitInfo info_) : dsvHandle(){
+	const DXGI_FORMAT dsvFormat = info_.resourceDesc->Format;
+
+	D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc{};
+	if(info_.resourceDesc->Format == DXGI_FORMAT_D32_FLOAT){
+		info_.resourceDesc->Format = DXGI_FORMAT_R32_TYPELESS;
+		srvDesc.Format = DXGI_FORMAT_R32_FLOAT;
+	}
+
+	srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
+	srvDesc.Texture2D.MipLevels = 1;
+	srvDesc.Texture2D.MostDetailedMip = 0;
+	srvDesc.Texture2D.PlaneSlice = 0;
+	srvDesc.Texture2D.ResourceMinLODClamp = 0.0f;
+
+	GADGET_BASIC_ASSERT(info_.srvDesc == nullptr);
+	GADGET_BASIC_ASSERT(info_.resource == nullptr);
+	info_.srvDesc = &srvDesc;
+
+	DX12_TextureInfo::DX12_TextureInfo(info_);
+
+	D3D12_DEPTH_STENCIL_VIEW_DESC dsvDesc{};
+	dsvDesc.ViewDimension = D3D12_DSV_DIMENSION_TEXTURE2D;
+	dsvDesc.Flags = D3D12_DSV_FLAG_NONE;
+	dsvDesc.Format = dsvFormat;
+	dsvDesc.Texture2D.MipSlice = 0;
+
+	dsvHandle = DX12::DSVHeap().Allocate();
+	DX12::MainDevice()->CreateDepthStencilView(resource, &dsvDesc, dsvHandle.cpuHandle);
+}
+
+DX12_DepthBufferTextureInfo::~DX12_DepthBufferTextureInfo(){
+	DX12::DSVHeap().Free(dsvHandle);
+}
---
+++ GadgetEngine/Graphics/DX12/DX12_TextureInfo.h
@@ -0,0 +1,67 @@
+#ifndef GADGET_DX12_TEXTURE_INFO_H
+#define GADGET_DX12_TEXTURE_INFO_H
+
+#include "DX12_DescriptorHeap.h"
+#include "Graphics/TextureInfo.h"
+
+namespace Gadget{
+	struct DX12_TextureInitInfo{
+		ID3D12Heap1* heap = nullptr;
+		ID3D12Resource* resource = nullptr;
+		D3D12_SHADER_RESOURCE_VIEW_DESC* srvDesc = nullptr;
+		D3D12_RESOURCE_DESC* resourceDesc = nullptr;
+		D3D12_RESOURCE_ALLOCATION_INFO1 allocationInfo{};
+		D3D12_RESOURCE_STATES initialState{};
+		D3D12_CLEAR_VALUE clearValue{};
+	};
+
+	class DX12_TextureInfo : public TextureInfo{
+	public:
+		constexpr static uint32_t maxMips = 14;
+
+		DX12_TextureInfo();
+		explicit DX12_TextureInfo(const DX12_TextureInitInfo& info_);
+		DISABLE_COPY_AND_MOVE(DX12_TextureInfo);
+		virtual ~DX12_TextureInfo() override;
+
+		virtual void Bind(int textureIndex_ = 0) override{ GADGET_ASSERT_NOT_IMPLEMENTED; }
+		virtual void Unbind() override{ GADGET_ASSERT_NOT_IMPLEMENTED; }
+
+		constexpr ID3D12Resource* const GetResource() const{ return resource; }
+		constexpr DX12_DescriptorHandle SRV() const{ return srvHandle; }
+
+	protected:
+		ID3D12Resource* resource;
+		DX12_DescriptorHandle srvHandle;
+	};
+
+	class DX12_RenderTextureInfo : public DX12_TextureInfo{
+	public:
+		DX12_RenderTextureInfo();
+		explicit DX12_RenderTextureInfo(const DX12_TextureInitInfo& info_);
+		DISABLE_COPY_AND_MOVE(DX12_RenderTextureInfo);
+		virtual ~DX12_RenderTextureInfo() override;
+
+		constexpr uint32_t MipCount() const{ return mipCount; }
+		D3D12_CPU_DESCRIPTOR_HANDLE RTV(uint32_t mipIndex_) const;
+
+	protected:
+		DX12_DescriptorHandle rtvHandles[maxMips];
+		uint32_t mipCount;
+	};
+
+	class DX12_DepthBufferTextureInfo : public DX12_TextureInfo{
+	public:
+		DX12_DepthBufferTextureInfo();
+		explicit DX12_DepthBufferTextureInfo(DX12_TextureInitInfo info_);
+		DISABLE_COPY_AND_MOVE(DX12_DepthBufferTextureInfo);
+		virtual ~DX12_DepthBufferTextureInfo() override;
+
+		constexpr D3D12_CPU_DESCRIPTOR_HANDLE DSV() const{ return dsvHandle.cpuHandle; }
+
+	protected:
+		DX12_DescriptorHandle dsvHandle;
+	};
+}
+
+#endif //!GADGET_DX12_TEXTURE_INFO_H
