48fc3e64b0bc8e4258c6eeb6199fc545f13474a1
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2024-04-29 18:47:51 -0400

    build-resources.bat stops if DataBuilder encounters an error

--- build-resources.bat
+++ build-resources.bat
@@ -3,6 +3,10 @@ xcopy /s "GadgetEngine\Graphics\DX12\Shaders" "Build\Resources\" /Y
 
 START /WAIT /B DataBuilder.lnk -f
 
+@echo off
+if %errorlevel%==-1 ( echo Data Build was not successful && PAUSE && EXIT /B 2)
+@echo on
+
 xcopy /s "Build\Resources\" "Build\Game\Debug\Resources" /Y /d
 xcopy /s "Build\Resources\" "Build\Game\Develop\Resources" /Y /d
 xcopy /s "Build\Resources\" "Build\Game\Release\Resources" /Y /d
