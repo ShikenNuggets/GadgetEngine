23b86ef421b1e7ba8211a39bae32dc8b6eee0459
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2024-04-29 21:52:06 -0400

    Support for generating a random color

--- GadgetEngine/Random.h
+++ GadgetEngine/Random.h
@@ -3,22 +3,32 @@
 
 #include <chrono>
 
+#include "Graphics/Color.h"
+
 namespace Gadget{
 	//TODO - Implement custom RNG instead of using rand (Mersenne Twister? Xorshift? KISS99?)
-	class Random{
-	public:
-		static void SetSeed(){
+	namespace Random{
+		inline void SetSeed(){
 			srand(static_cast<unsigned int>(std::chrono::high_resolution_clock::now().time_since_epoch().count()));
 		}
 		
 		template <class T>
-		static T Range(T min_, T max_){
+		inline T Range(T min_, T max_){
 			return rand() % max_ + min_;
 		}
 
 		template<>
-		float Range(float min_, float max_){
+		inline float Range(float min_, float max_){
 			return min_ + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (max_ - min_)));
+		}
+
+		inline Color RandomColor(bool randomAlpha_ = false){
+			float alpha = 1.0f;
+			if(randomAlpha_){
+				alpha = Range(0.0f, 1.0f);
+			}
+
+			return Color(Range(0.0f, 1.0f), Range(0.0f, 1.0f), Range(0.0f, 1.0f), alpha);
 		}
 	};
 }
