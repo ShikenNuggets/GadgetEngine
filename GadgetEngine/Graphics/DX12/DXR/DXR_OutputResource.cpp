5cc724e8b75cab5c0fb77dd6984fd8e6292d51f0
ShikenNuggets <7827560+ShikenNuggets@users.noreply.github.com>
2024-05-06 10:13:16 -0400

    DXR_OutputResource class

---
+++ GadgetEngine/Graphics/DX12/DXR/DXR_OutputResource.cpp
@@ -0,0 +1,28 @@
+#include "DXR_OutputResource.h"
+
+#include "Debug.h"
+#include "Graphics/DX12/DX12.h"
+
+using namespace Gadget;
+
+DXR_OutputResource::DXR_OutputResource(const ScreenCoordinate& frameSize_) : outputResource(nullptr){
+	GADGET_BASIC_ASSERT(DX12::IsInstanceInitialized());
+
+	D3D12_RESOURCE_DESC resDesc = {};
+	resDesc.DepthOrArraySize = 1;
+	resDesc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
+	resDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	resDesc.Flags = D3D12_RESOURCE_FLAG_ALLOW_UNORDERED_ACCESS;
+	resDesc.Width = frameSize_.x;
+	resDesc.Height = frameSize_.y;
+	resDesc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
+	resDesc.MipLevels = 1;
+	resDesc.SampleDesc.Count = 1;
+
+	HRESULT hr = DX12::GetInstance().MainDevice()->CreateCommittedResource(&DX12_Helpers::DefaultHeapProperties, D3D12_HEAP_FLAG_NONE, &resDesc, D3D12_RESOURCE_STATE_COPY_SOURCE, nullptr, IID_PPV_ARGS(outputResource.ReleaseAndGetAddressOf()));
+	if(FAILED(hr) || outputResource == nullptr){
+		Debug::ThrowFatalError(SID("RENDER"), "Could not create output resource!", ErrorCode::D3D12_Error, __FILE__, __LINE__);
+	}
+
+	outputResource->SetName(L"DXR Output Resource");
+}
---
+++ GadgetEngine/Graphics/DX12/DXR/DXR_OutputResource.h
@@ -0,0 +1,20 @@
+#ifndef GADGET_DXR_OUTPUT_RESOURCE_H
+#define GADGET_DXR_OUTPUT_RESOURCE_H
+
+#include <d3d12.h>
+#include <wrl/client.h>
+
+#include "ScreenCoordinate.h"
+#include "Graphics/DX12/DX12_Defines.h"
+
+namespace Gadget{
+	class DXR_OutputResource{
+	public:
+		DXR_OutputResource(const ScreenCoordinate& frameSize_);
+
+	private:
+		Microsoft::WRL::ComPtr<ID3D12_Resource> outputResource;
+	};
+}
+
+#endif //!GADGET_DXR_OUTPUT_RESOURCE_H
