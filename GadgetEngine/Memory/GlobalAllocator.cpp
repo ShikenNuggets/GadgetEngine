a8697b7b581b0a4de741547f230a17f48f7d7bf6
ShikenNuggets <carter.rennick@gmail.com>
2024-07-12 22:35:19 -0400

    Profiling memory allocations

--- GadgetEngine/Memory/GlobalAllocator.cpp
+++ GadgetEngine/Memory/GlobalAllocator.cpp
@@ -3,26 +3,41 @@
 #include <new>
 #include <iostream>
 
+#include "Utils/Profiler.h"
+
 using namespace Gadget;
 
 std::atomic<uint64_t> GlobalAllocator::numAllocs = 0;
 std::atomic<uint64_t> GlobalAllocator::numFrees = 0;
+std::atomic<double> GlobalAllocator::allocTime = 0.0;
+std::atomic<double> GlobalAllocator::freeTime = 0.0;
+
+static StringID MemAllocSID = SID("MemAlloc");
+static StringID MemFreeSID = SID("MemFree");
 
 #pragma warning(disable : 28251)
 
 void* operator new(size_t size_) noexcept(false){
+	auto startTime = Timing::CurrentTime();
+
 	void* ptr = malloc(size_);
 	if(ptr == nullptr){
 		throw std::bad_alloc();
 	}
 
-	GlobalAllocator::numAllocs++;
+	GlobalAllocator::AddToAllocCount();
+	GlobalAllocator::AddAllocTime(Timing::TimeSince(startTime));
+
 	return ptr;
 }
 
 #pragma warning(default : 28251)
 
 void operator delete(void* p_) noexcept{
-	GlobalAllocator::numFrees++;
+	auto startTime = Timing::CurrentTime();
+
 	free(p_);
+	
+	GlobalAllocator::AddToFreeCount();
+	GlobalAllocator::AddFreeTime(Timing::TimeSince(startTime));
 }
--- GadgetEngine/Memory/GlobalAllocator.h
+++ GadgetEngine/Memory/GlobalAllocator.h
@@ -6,8 +6,23 @@
 namespace Gadget{
 	class GlobalAllocator{
 	public:
+		//DO NOT call these directly
+		static void AddToAllocCount(){ numAllocs++; }
+		static void AddToFreeCount(){ numFrees++; }
+		static void AddAllocTime(double time_){ allocTime += time_; }
+		static void AddFreeTime(double time_){ freeTime += time_; }
+
+		static uint64_t GetNumAllocs(){ return numAllocs; }
+		static uint64_t GetNumFrees(){ return numFrees; }
+		static double GetAllocTime(){ return allocTime; }
+		static double GetFreeTime(){ return freeTime; }
+
+	private:
 		static std::atomic<uint64_t> numAllocs;
 		static std::atomic<uint64_t> numFrees;
+
+		static std::atomic<double> allocTime;
+		static std::atomic<double> freeTime;
 	};
 }
 
