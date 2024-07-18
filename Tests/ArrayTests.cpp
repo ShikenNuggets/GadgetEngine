7d705e583595eafc9ec263ecd7282f1e5528f1de
ShikenNuggets <carter.rennick@gmail.com>
2024-07-17 21:07:01 -0400

    More Array tests

--- Tests/ArrayTests.cpp
+++ Tests/ArrayTests.cpp
@@ -118,3 +118,98 @@ TEST_CASE("Array Accessor" "[array_access]"){
 	REQUIRE(matrix[2][1] == 5);
 	REQUIRE(matrix[2][2] == 2);
 }
+
+//------------------------------------------------------------//
+//--------------- Zero Matrix (CTCI 1.8) ---------------------//
+//------------------------------------------------------------//
+//O(rows * cols)
+template <typename T>
+static inline void ZeroMatrix(Array<Array<T>>& inMatrix_){
+	Array<size_t> rows;
+	Array<size_t> cols;
+
+	for(size_t i = 0; i < inMatrix_.Size(); i++){
+		for(size_t j = 0; j < inMatrix_[i].Size(); j++){
+			if(inMatrix_[i][j] == 0){
+				rows.Add(j);
+				cols.Add(i);
+			}
+		}
+	}
+
+	for(size_t i = 0; i < rows.Size(); i++){
+		for(size_t j = 0; j < inMatrix_.Size(); j++){
+			inMatrix_[j][rows[i]] = 0;
+		}
+	}
+
+	for(size_t i = 0; i < cols.Size(); i++){
+		for(size_t j = 0; j < inMatrix_[cols[i]].Size(); j++){
+			inMatrix_[cols[i]][j] = 0;
+		}
+	}
+}
+
+TEST_CASE("Array Zero Matrix", "[array_zero_matrix]"){
+	Array<Array<int>> matrix;
+	matrix.Add(Array<int>());
+	matrix.Add(Array<int>());
+	matrix.Add(Array<int>());
+
+	matrix[0].Add(1);
+	matrix[0].Add(1);
+	matrix[0].Add(1);
+	matrix[0].Add(1);
+
+	matrix[1].Add(0);
+	matrix[1].Add(1);
+	matrix[1].Add(0);
+	matrix[1].Add(1);
+
+	matrix[2].Add(1);
+	matrix[2].Add(1);
+	matrix[2].Add(1);
+	matrix[2].Add(1);
+
+	REQUIRE(matrix.Size() == 3);
+	REQUIRE(matrix[0].Size() == 4);
+	REQUIRE(matrix[1].Size() == 4);
+	REQUIRE(matrix[2].Size() == 4);
+
+	REQUIRE(matrix[0][0] == 1);
+	REQUIRE(matrix[0][1] == 1);
+	REQUIRE(matrix[0][2] == 1);
+	REQUIRE(matrix[0][3] == 1);
+
+	REQUIRE(matrix[1][0] == 0);
+	REQUIRE(matrix[1][1] == 1);
+	REQUIRE(matrix[1][2] == 0);
+	REQUIRE(matrix[1][3] == 1);
+
+	REQUIRE(matrix[2][0] == 1);
+	REQUIRE(matrix[2][1] == 1);
+	REQUIRE(matrix[2][2] == 1);
+	REQUIRE(matrix[2][3] == 1);
+
+	ZeroMatrix(matrix);
+
+	REQUIRE(matrix.Size() == 3);
+	REQUIRE(matrix[0].Size() == 4);
+	REQUIRE(matrix[1].Size() == 4);
+	REQUIRE(matrix[2].Size() == 4);
+
+	REQUIRE(matrix[0][0] == 0);
+	REQUIRE(matrix[0][1] == 1);
+	REQUIRE(matrix[0][2] == 0);
+	REQUIRE(matrix[0][3] == 1);
+
+	REQUIRE(matrix[1][0] == 0);
+	REQUIRE(matrix[1][1] == 0);
+	REQUIRE(matrix[1][2] == 0);
+	REQUIRE(matrix[1][3] == 0);
+
+	REQUIRE(matrix[2][0] == 0);
+	REQUIRE(matrix[2][1] == 1);
+	REQUIRE(matrix[2][2] == 0);
+	REQUIRE(matrix[2][3] == 1);
+}
