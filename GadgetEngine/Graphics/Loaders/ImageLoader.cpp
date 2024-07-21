27c2791384798515a33b592033d396a066170b65
ShikenNuggets <carter.rennick@gmail.com>
2024-07-21 18:26:19 -0400

    Fixed ImageLoader bugs

--- GadgetEngine/Graphics/Loaders/ImageLoader.cpp
+++ GadgetEngine/Graphics/Loaders/ImageLoader.cpp
@@ -5,11 +5,17 @@
 using namespace Gadget;
 
 Texture* ImageLoader::LoadImage(const std::string& filePath_){
-	IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG);
+	static constexpr int flags = IMG_INIT_JPG | IMG_INIT_PNG;
+	int init = IMG_Init(flags);
+	if((init & flags) != flags){
+		GADGET_ASSERT(false, std::string("SDL_Image init failed. SDL Error: ") + SDL_GetError());
+		return nullptr;
+	}
+
 	SDL_Surface* surface = IMG_Load(filePath_.c_str());
 	GADGET_BASIC_ASSERT(surface != nullptr);
 	if(surface == nullptr){
-		GADGET_LOG_WARNING(SID("RENDER"), "An error occurred on loading image " + filePath_);
+		GADGET_LOG_WARNING(SID("RENDER"), "An error occurred on loading " + filePath_ + ". SDL Error: " + SDL_GetError());
 		return nullptr;
 	}
 
@@ -18,5 +24,5 @@ Texture* ImageLoader::LoadImage(const std::string& filePath_){
 		pixelData.push_back(reinterpret_cast<uint8_t*>(surface->pixels)[i]);
 	}
 
-	return new Texture(surface->w, surface->h, surface->format->BytesPerPixel, pixelData);
+	return new Texture(surface->w, surface->h, surface->format->BitsPerPixel, pixelData);
 }
