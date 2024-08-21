eff181986b711b581fc2b7cc903e774620724409
ShikenNuggets <carter.rennick@gmail.com>
2024-08-20 21:50:29 -0400

    Animation specific data structures

---
+++ GadgetEngine/Animation/AnimMesh.h
@@ -0,0 +1,52 @@
+#ifndef GADGET_ANIMATION_ANIM_MESH_H
+#define GADGET_ANIMATION_ANIM_MESH_H
+
+#include <cstdint>
+
+#include "Animation/AnimVertex.h"
+#include "Animation/Skeleton.h"
+#include "Core/FileSystem.h"
+#include "Data/Array.h"
+#include "Debug.h"
+#include "Graphics/Vertex.h"
+#include "Graphics/Loaders/AssimpModelLoader.h"
+#include "Resource/Resource.h"
+
+namespace Gadget{
+	struct AnimSubmesh{
+		AnimSubmesh(const Array<AnimVertex>& verts_, const Array<uint32_t>& indices_) : vertices(verts_), indices(indices_){}
+		AnimSubmesh(const AnimSubmesh& other_) : vertices(other_.vertices), indices(other_.indices){}
+
+		const Array<AnimVertex> vertices;
+		const Array<uint32_t> indices;
+	};
+
+	class AnimMesh : public Resource{
+	public:
+		AnimMesh(const Array<AnimSubmesh>& submeshes_, const Skeleton& skeleton_, const Matrix4& globalInverse_) : submeshes(submeshes_), skeleton(skeleton_), globalInverse(globalInverse_){}
+
+		static constexpr const char* typeName = "AnimMesh";
+
+		const Array<AnimSubmesh> submeshes;
+		const Skeleton skeleton;
+		const Matrix4 globalInverse;
+	};
+
+	class AnimMeshResourceContainer : public ResourceContainer{
+	public:
+		AnimMeshResourceContainer(const std::string& path_) : ResourceContainer(AnimMesh::typeName, path_){
+			GADGET_BASIC_ASSERT(!path_.empty());
+			GADGET_BASIC_ASSERT(FileSystem::FileExists(path_));
+		}
+
+		virtual Resource* LoadResource() override{
+			return AssimpModelLoader::LoadAnimMesh(path);
+		}
+
+		virtual const std::type_info& GetResourceTypeInfo() override{
+			return typeid(AnimMesh);
+		}
+	};
+}
+
+#endif //!GADGET_ANIMATION_ANIM_MESH_H
---
+++ GadgetEngine/Animation/AnimVertex.h
@@ -0,0 +1,23 @@
+#ifndef GADGET_ANIMATION_ANIM_VERTEX_H
+#define GADGET_ANIMATION_ANIM_VERTEX_H
+
+#include "Data/StaticArray.h"
+#include "Graphics/Vertex.h"
+#include "Math/Vector.h"
+
+namespace Gadget{
+	struct AnimVertex{
+		AnimVertex(const Vector3& pos_, const Vector3& normal_, const Vector2& texCoords_) : vertex(pos_, normal_, texCoords_){
+			jointIDs.Reserve(maxJointWeights);
+			jointWeights.Reserve(maxJointWeights);
+		}
+
+		static constexpr int maxJointWeights = 4;
+
+		Vertex vertex;
+		Array<int32_t> jointIDs;
+		Array<float> jointWeights;
+	};
+}
+
+#endif //!GADGET_ANIMATION_ANIM_VERTEX_H
