9583bbdf9f12122eb76e292c0388c1178f98ec92
ShikenNuggets <carter.rennick@gmail.com>
2024-08-22 16:57:10 -0400

    More keyframe improvements

--- GadgetEngine/Animation/KeyFrame.h
+++ GadgetEngine/Animation/KeyFrame.h
@@ -23,31 +23,31 @@ namespace Gadget{
 
 	template <class T1, class T2>
 	struct ClipSearchResult{
-		explicit ClipSearchResult(T2* node_ = nullptr) : result(), node(node_){
+		explicit ClipSearchResult(const T2* node_ = nullptr) : result(), node(node_){
 			if(node_ != nullptr){
-				result = node_->value;
+				result = node_->value.value;
 			}
 		}
 
 		T1 result;
-		T2* node;
+		const T2* node;
 	};
 
-	using VectorResult = ClipSearchResult<VectorKey, VectorNode>;
-	using QuatResult = ClipSearchResult<QuatKey, QuatNode>;
+	using VectorResult = ClipSearchResult<Vector3, VectorNode>;
+	using QuatResult = ClipSearchResult<Quaternion, QuatNode>;
 
 	struct FullClipSearchResult{
 		FullClipSearchResult(const VectorResult& pos_, const QuatResult& rot_, const VectorResult& scale_) : posNode(pos_.node), rotNode(rot_.node), scaleNode(scale_.node){
 			result = Matrix4::Identity();
-			result *= Matrix4::Translate(pos_.result.value);
-			result *= rot_.result.value.ToMatrix4();
-			result *= Matrix4::Scale(scale_.result.value);
+			result *= Matrix4::Translate(pos_.result);
+			result *= rot_.result.ToMatrix4();
+			result *= Matrix4::Scale(scale_.result);
 		}
 
 		Matrix4 result;
-		VectorNode* posNode;
-		QuatNode* rotNode;
-		VectorNode* scaleNode;
+		const VectorNode* posNode;
+		const QuatNode* rotNode;
+		const VectorNode* scaleNode;
 	};
 }
 
