43b824926c9a86c158e83f6a15b81e1c6a158357
ShikenNuggets <carter.rennick@gmail.com>
2024-08-23 09:14:23 -0400

    Stack data structure implementation

---
+++ GadgetEngine/Data/Stack.h
@@ -0,0 +1,48 @@
+#ifndef GADGET_DATA_STACK_H
+#define GADGET_DATA_STACK_H
+
+#include "Debug.h"
+#include "Data/List.h"
+
+namespace Gadget{
+	template <class T>
+	class Stack{
+	public:
+		Stack() : data(){}
+		~Stack() = default;
+
+		void Push(const T& item_){
+			data.AddFront(item_);
+		}
+
+		T Pop(){
+			GADGET_BASIC_ASSERT(!data.IsEmpty());
+			if(data.IsEmpty()){
+				Debug::ThrowFatalError(SID("DATA"), "Called Pop on an empty stack!", ErrorCode::Invalid_State, __FILE__, __LINE__);
+			}
+
+			GADGET_BASIC_ASSERT(data.Front() != nullptr);
+			T value = data.Front()->value;
+			data.PopFront();
+			return value;
+		}
+
+		T Peek() const{
+			GADGET_BASIC_ASSERT(!data.IsEmpty());
+			if(data.IsEmpty()){
+				Debug::ThrowFatalError(SID("DATA"), "Called Peek on an empty stack!", ErrorCode::Invalid_State, __FILE__, __LINE__);
+			}
+
+			GADGET_BASIC_ASSERT(data.Front() != nullptr);
+			return data.Front()->value;
+		}
+
+		bool IsEmpty() const{ return data.IsEmpty(); }
+		size_t Size() const{ return data.Size(); }
+
+	private:
+		List<T> data;
+	};
+}
+
+#endif //!GADGET_DATA_STACK_H
