20f84c5bd9551d5890c78584c4b3db1d27f95597
ShikenNuggets <carter.rennick@gmail.com>
2024-10-02 16:35:57 -0400

    Workbench - Open VS solution from the title bar

--- WorkbenchCPP/Windows/EditorToolbar.cpp
+++ WorkbenchCPP/Windows/EditorToolbar.cpp
@@ -2,6 +2,8 @@
 
 #include <imgui.h>
 
+#include <Platform/Windows/Win32_Utils.h>
+
 #include "Windows/Tabs/ResourcesTab.h"
 
 using namespace Gadget;
@@ -33,6 +35,36 @@ void EditorToolbar::Draw(){
 
 			if(ImGui::MenuItem("Resources")){
 				currentTab = Tab::Resources;
+			}
+
+			if(ImGui::MenuItem("Code")){
+				uint64_t hwnd = Win32_Utils::GetWindowOfRunningApplication("Microsoft Visual Studio", "Gadget"); //TODO - Harcoded for testing purposes
+				if(hwnd == NULL){
+					const ErrorCode err = Win32_Utils::OpenFileInDefaultApplication("E:\\Coding\\GadgetEngine\\GadgetEngine.sln"); //TODO - Harcoded for testing purposes
+					if(err == ErrorCode::Win32_FileIO_PermissionsError){
+						Debug::PopupErrorMessage("File Permissions Error", "Workbench does not have permission to open the Visual Studio solution. Try running Workbench as administrator.");
+					}else if(err == ErrorCode::Win32_NoAssociationError){
+						Debug::PopupErrorMessage("No Associated Program Error", "You do not have a default program set in Windows for opening Visual Studio solutions.");
+					}else if(err != ErrorCode::OK){
+						Debug::PopupErrorMessage("Error", "Could not open Visual Studio. If the issue persists, please contact the developer of Gadget Engine/Workbench. Error Code: " + GetErrorCodeString(err));
+					}
+				}else{
+					ErrorCode err = Win32_Utils::ShowWindow(hwnd);
+					GADGET_BASIC_ASSERT(err != ErrorCode::Invalid_Args);
+					if(err != ErrorCode::OK){
+						Debug::PopupErrorMessage("Error", "Could not focus show the existing Visual Studio window. If the issue persists, please contact the developer of Gadget Engine/Workbench. Error Code: " + GetErrorCodeString(err));
+					}
+
+					err = Win32_Utils::BringWindowToForeground(hwnd);
+					GADGET_BASIC_ASSERT(err != ErrorCode::Invalid_Args);
+					if(err != ErrorCode::OK){
+						Debug::PopupErrorMessage("Error", "Could not focus bring the existing Visual Studio window to the foreground. If the issue persists, please contact the developer of Gadget Engine/Workbench. Error Code: " + GetErrorCodeString(err));
+					}
+				}
+			}
+
+			if(ImGui::MenuItem("Play")){
+				//TODO - Launch game
 			}
 
 			ImGui::EndMenuBar();
