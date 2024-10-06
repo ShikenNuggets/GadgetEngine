34485df1d174d75d5f57ebfabde4160d43dceb73
ShikenNuggets <carter.rennick@gmail.com>
2024-10-06 14:25:34 -0400

    Copy premake folder into WorkbenchCPP build

--- premake5.lua
+++ premake5.lua
@@ -211,6 +211,7 @@ project "WorkbenchCPP"
 			"echo D|xcopy \"$(ProjectDir)..\\SDK\\_Gadget\\lib\\$(Configuration)\\*.dll\" \"$(TargetDir)\" /y /E /d",
 			"echo D|xcopy \"$(ProjectDir)..\\SDK\\_Gadget\\lib\\$(Configuration)\\*.pdb\" \"$(TargetDir)\" /y /E /d",
 			"echo D|xcopy \"$(ProjectDir)..\\Resources\\*.ttf\" \"$(TargetDir)Resources\\Fonts\\\" /y /E /d",
+			"echo D|xcopy \"$(ProjectDir)..\\premake\\*.*\" \"$(TargetDir)premake\\\" /y /E /d",
 		}
 
 	filter "configurations:Debug or Develop"
