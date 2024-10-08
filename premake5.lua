c7cdb21a6a6d5ff9eac7ab9bb96802f92eec14ac
ShikenNuggets <carter.rennick@gmail.com>
2024-10-07 21:45:38 -0400

    Workbench - Copy shaders

--- premake5.lua
+++ premake5.lua
@@ -224,6 +224,7 @@ project "WorkbenchCPP"
 			"echo D|xcopy \"$(ProjectDir)..\\SDK\\_Gadget\\lib\\*.*\" \"$(TargetDir)SDK\\_Gadget\\lib\\\" /y /E /d",
 			"echo D|xcopy \"$(ProjectDir)..\\SDK\\_Gadget\\include\\*.*\" \"$(TargetDir)SDK\\_Gadget\\include\\\" /y /E /d",
 			"echo D|xcopy \"$(ProjectDir)..\\Resources\\*.ttf\" \"$(TargetDir)Resources\\Fonts\\\" /y /E /d",
+			"echo D|xcopy \"$(ProjectDir)..\\Resources\\Shaders\\*.*\" \"$(TargetDir)Resources\\Shaders\\\" /y /E /d",
 			"echo D|xcopy \"$(ProjectDir)..\\premake\\*.*\" \"$(TargetDir)premake\\\" /y /E /d",
 			"echo D|xcopy \"$(ProjectDir)*.template\" \"$(TargetDir)\" /y /d",
 		}
