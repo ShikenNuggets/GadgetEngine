51cb20aa3bbd522cab8c8a676c1413958bd6f1a2
ShikenNuggets <carter.rennick@gmail.com>
2025-05-11 11:16:59 -0400

    Cleanup

--- GadgetEngine/Platform/Windows/Win32_Utils.h
+++ GadgetEngine/Platform/Windows/Win32_Utils.h
@@ -13,25 +13,25 @@ namespace Gadget{
 			const wchar_t* subTitle;
 		};
 
-		std::string GetUserDocumentsPath();
+		extern std::string GetUserDocumentsPath();
 
-		void SetConsoleColorWhite();
-		void SetConsoleColorRed();
-		void SetConsoleColorYellow();
-		void SetConsoleColorGreen();
-		void SetConsoleColorBlue();
+		extern void SetConsoleColorWhite();
+		extern void SetConsoleColorRed();
+		extern void SetConsoleColorYellow();
+		extern void SetConsoleColorGreen();
+		extern void SetConsoleColorBlue();
 
-		void OutputToDebuggerConsole(const std::string& output);
+		extern void OutputToDebuggerConsole(const std::string& output);
 
-		void TryApplyImmersiveDarkMode(uint64_t hwnd_);
+		extern void TryApplyImmersiveDarkMode(uint64_t hwnd_);
 
-		std::string BrowseForFolder(uint64_t hwnd_, const wchar_t* dialogTitle_);
+		extern std::string BrowseForFolder(uint64_t hwnd_, const wchar_t* dialogTitle_);
 
-		ErrorCode ShowWindow(uint64_t hwnd_);
-		ErrorCode BringWindowToForeground(uint64_t hwnd_);
-		ErrorCode OpenFileInDefaultApplication(const std::string& filePath_, const std::string& args_ = "");
+		extern ErrorCode ShowWindow(uint64_t hwnd_);
+		extern ErrorCode BringWindowToForeground(uint64_t hwnd_);
+		extern ErrorCode OpenFileInDefaultApplication(const std::string& filePath_, const std::string& args_ = "");
 
-		uint64_t GetWindowOfRunningApplication(const std::string& windowName_, const std::string& subTitle_ = "");
+		extern uint64_t GetWindowOfRunningApplication(const std::string& windowName_, const std::string& subTitle_ = "");
 
 		extern std::wstring ToWString(const std::string& str_);
 		extern std::string WStringToString(const std::wstring& wstr_);
